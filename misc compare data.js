b20b56e8f9babdfceff3e314dad29dd38e262fd5
William Swartz <superderpicus@gmail.com>
2020-09-04 02:54:01 -0600

    Update misc compare data.js

--- misc compare data.js
+++ misc compare data.js
@@ -196,3 +196,91 @@ function calcBlast()
     }
 
 }
+
+// onOpen function:
+// create dropdown menu when loaded
+function onOpen() 
+{
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('Presets')
+       .addSubMenu(ui.createMenu('Non-Tenderized')
+          .addItem('will', 'clear')
+          .addItem('add', 'safiMeta')
+          .addItem('later', 'brachyMeta')
+          .addItem('but', 'safiMax')
+          .addItem('im', 'noTendy')
+          .addItem('lazy', 'kPara'))
+      .addSubMenu(ui.createMenu('Tenderized')
+          .addItem('Folicath 3 Forz', 'folicath')
+          .addItem('Vezirstag 3 Forz', 'vezir')
+          .addItem('Valorwing 3 Medis', 'valorwing')
+          .addItem('Gleambeetle 3 Velox', 'gleambeetle')
+          .addItem('Nexus Dragon Soul', 'dragonsoul'))
+       .addSeparator()
+       .addItem('Reset Values', 'reset')
+      .addToUi();
+}
+
+var totalMV = 1.29;
+var totalEle = 4.8;
+var animationTime = 4.22; 
+
+var dtTotalMV = 1.79;
+var dtAnimationTime = 6.07;
+
+var sets =
+[
+    ['Safi Shatterspear (100% Coal)',   1002.19, 0, 7.33, 13],
+    ['Safi Shatterspear (75% Coal)',    996.35, 0, 7.33, 14],
+    ['Safi Shatterspear (50% Coal)',    988.57, 0, 7.33, 15],
+    ['Safi Shatterspear (25% Coal)',    982.73, 0, 7.33, 16],
+    ['Safi Shatterspear (0% Coal)',     978.84, 0, 7.33, 17],
+    ['Lightbreak Press (100% Coal)',    990.51, 0, 7.33, 18],
+    ['Lightbreak Press (75% Coal)',     984.68, 0, 7.33, 19],
+    ['Lightbreak Press (50% Coal)',     978.84, 0, 7.33, 20],
+    ['Lightbreak Press (25% Coal)',     971.05, 0, 7.33, 21],
+    ['Lightbreak Press (0% Coal)',      965.22, 0, 7.33, 22],
+    ['Alatreon\'s Embrace',             969.11, 85, 0, 23],
+    ['Safi Raw Ele (100% Coal)',        1002.19, 26.25, 0, 24],
+    ['Safi Raw Ele (75% Coal)',         996.35, 25.31, 0, 25],
+    ['Safi Raw Ele (50% Coal)',         988.57, 24.38, 0, 26],
+    ['Safi Raw Ele (25% Coal)',         982.73, 23.44, 0, 27],
+    ['Safi Raw Ele (0% Coal)',          974.95, 22.5, 0, 28],
+    ['KPara',                           911.06, 0, 0, 29],
+    ['KWater',                          872.26, 97.81, 0, 30],
+    ['Safihemoth',                      945.76, 50, 0, 31],
+    ['Brakulve (100% Coal)',            980.78, 35, 0, 32],
+    ['Brakulve (75% Coal)',             973, 34.06, 0, 33],
+    ['Brakulve (50% Coal)',             967.16, 33.13, 0, 34],
+    ['Brakulve (25% Coal)',             959.38, 32.19, 0, 35],
+    ['Brakulve (0% Coal)',              953.54, 31.25, 0, 36],
+]
+
+function setValue(index, loop)
+{
+
+    var sheet = SpreadsheetApp.getActiveSheet(); // current input sheet
+    if (loop == 0)
+    {
+        var values = sets[index];
+        sheet.getRange('A' + values[4] + ':F' + values[4]).setValues([[values[0], values[1], values[2], values[3], '=((' + totalMV + ') * B' + values[4] + ' * ($B$3/100) + (' + totalEle + '*(C' + values[4] + '/100)*$B$4))', '=E' + values[4] + '/' + animationTime]]);
+    }
+    else
+    {
+        var values = sets[index];
+        sheet.getRange('H' + values[4] + ':M' + values[4]).setValues([[values[0], values[1], values[2], values[3], '=((' + dtTotalMV + ') * I' + values[4] + ' * ($B$3/100) + (' + totalEle + '*(J' + values[4] + '/100)*$B$4)) + ($B$8*Data!$C$12)', '=L' + values[4] + '/' + dtAnimationTime]]);
+    }
+}
+
+function reset()
+{
+
+    var sheet = SpreadsheetApp.getActiveSheet(); // current input sheet
+    for (var x = 0; x < sets.length; x++)
+    {
+        setValue(x, 0);
+        setValue(x, 1);
+    }
+
+    sheet.getRange('B3:B8').setValues([[80], [30], [19], [20], ['Regular'], [3]]);
+}
